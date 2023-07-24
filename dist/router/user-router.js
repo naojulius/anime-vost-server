c9c6f80f964a78bd050aa58b9224f41e2c9c22c2
naojulius <naoj8lius@gmail.com>
2023-07-24 11:42:00 +0300

    delete: authentication

--- dist/router/user-router.js
+++ dist/router/user-router.js
@@ -5,7 +5,7 @@ var middlewares_1 = require("../middlewares");
 exports.default = (function (router) {
     router.post("/auth/register", user_controller_1.Register);
     router.post("/auth/login", user_controller_1.Login);
-    router.get("/user/all", middlewares_1.isAuthenticated, user_controller_1.GetAllUsers);
+    router.get("/user/all", user_controller_1.GetAllUsers);
     router.get("/user/delete/:id", middlewares_1.isAuthenticated, middlewares_1.isOwner, user_controller_1.GetAllUsers);
     router.get("/user/update/:id", middlewares_1.isAuthenticated, middlewares_1.isOwner, user_controller_1.UpdateUser);
 });
