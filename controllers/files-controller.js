b08039bb995cc7bf8d39186820e73db2d2042964
naojulius <naoj8lius@gmail.com>
2023-07-24 13:25:27 +0300

    update: index.js

--- controllers/files-controller.js
+++ controllers/files-controller.js
@@ -57,7 +57,7 @@ var GetImage = function (req, res) { return __awaiter(void 0, void 0, void 0, fu
                     contentType_1 = 'image/jpeg';
                     break;
             }
-            console.log(filePath)
+            eturn [2 /*return*/, res.sendStatus(200).json({ data: filePath })];
             if (!fs.existsSync(filePath)) {
                 return [2 /*return*/, res.status(404).json({ data: 'OMFG file not found' })];
             }
