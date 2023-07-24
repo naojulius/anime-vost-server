91ec75c87175dd04dd63cf526e54686142987c6a
naojulius <naoj8lius@gmail.com>
2023-07-24 12:00:14 +0300

    update: index.js

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-var express = require("express");
-var http = require("http");
-var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-var compression = require("compression");
-var cors = require("cors");
-var app = express();
-app.use(cors({
-    credentials: true
-}));
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json({ limit: '300mb' }));
+// index.js
+const express = require('express')
 
 const app = express()
 const PORT = 4000
@@ -22,8 +11,5 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
-
-
-
 // Export the Express API
 module.exports = app
